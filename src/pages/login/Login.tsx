acc1b4bbeb16e253015d9c5d62a6a5f811c7ce00
andreabocchimpani <hackernatsu93@gmail.com>
2025-07-19 17:33:42 +0200

    Firebase first build and deploy

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { signInWithPopup, getRedirectResult } from 'firebase/auth';
+import { signInWithPopup } from 'firebase/auth';
 import { useFirebase } from '../../context/firebase.context';
 import './Login.scss';
 
